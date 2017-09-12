b6fcc14f47255fc8f7682b30e64af744e8d196ab
secile <secile@cd6.so-net.ne.jp>
2017-09-12 23:45:00 +0900

    Add files via upload

---
+++ MjpegWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GitHub.secile.Avi
+{
+    class MjpegWriter
+    {
+        private AviWriter aviWriter;
+
+        public MjpegWriter(System.IO.Stream outputAvi, int width, int height, int fps)
+        {
+            aviWriter = new AviWriter(outputAvi, "MJPG", width, height, fps);
+        }
+
+        public void AddImage(Bitmap bmp)
+        {
+            using (var ms = new System.IO.MemoryStream())
+            {
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                aviWriter.AddImage(ms.GetBuffer());
+            }
+        }
+
+        public void Close()
+        {
+            aviWriter.Close();
+        }
+
+    }
+}
